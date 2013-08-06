1a6d3340d278de98e1bf03b26e68f4bd05c4914b
evgenitochev <evgenitochev@gmail.com>
2013-08-06 18:55:24 +0300

    homework
    homework

---
+++ C#2/1. Arrays/Arrays/20.Variations/Program.cs
@@ -0,0 +1,45 @@
+﻿//Write a program that reads two numbers N and K and generates all the variations of K elements from the set [1..N].
+//Example: N = 3, K = 2  {1, 1}, {1, 2}, {1, 3}, {2, 1}, {2, 2}, {2, 3}, {3, 1}, {3, 2}, {3, 3}
+
+
+using System;
+
+class Variations
+{
+    private static void GenerateVariations(int[] array, int elements, int numbers)
+    {
+        if (elements == -1)
+        {
+            Print(array);
+        }
+        else
+        {
+            for (int i = 1; i <= numbers; i++)
+            {
+                array[elements] = i;
+                GenerateVariations(array, elements - 1, numbers);
+            }
+        }
+    }
+
+    private static void Print(int[] array)
+    {
+        Console.Write("{ ");
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write("{0} ", array[i]);
+        }
+        Console.WriteLine("}");
+    }
+
+    static void Main()
+    {
+        int numbers = int.Parse(Console.ReadLine());
+        int elements = int.Parse(Console.ReadLine());
+
+        int[] array = new int[elements];
+
+        GenerateVariations(array, elements - 1, numbers);
+
+    }
+}
---
+++ C#2/2. Multidimensional-Arrays/MultidimensionalArrays/01. Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿//Write a program that fills and prints a matrix of size (n, n) as shown
+
+using System;
+
+class MatrixA
+{
+
+    static void Main()
+    {
+        int n = 4;
+        int[,] matrix = new int[n, n];
+
+        for (int i = 0, counter = 1; i < n; i++)
+        {
+            for (int j = 0; j < n; j++) matrix[j, i] = counter++;
+        }
+
+        int cellSize = (int)Math.Log10(matrix.Length) + 1;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(Convert.ToString(matrix[i, j]).PadRight(cellSize, ' ') + (j != matrix.GetLength(1) - 1 ? " " : "\n"));
+            }
+    }
+}
