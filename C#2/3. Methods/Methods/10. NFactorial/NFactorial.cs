dee9a900e871f2a28232c7cf0730bffbfe24c00f
evgenitochev <evgenitochev@gmail.com>
2013-08-06 18:56:29 +0300

    homework
    homework

---
+++ C#2/3. Methods/Methods/10. NFactorial/NFactorial.cs
@@ -0,0 +1,28 @@
+﻿//Write a program to calculate n! for each n in the range [1..100]. 
+//Hint: Implement first a method that multiplies a number represented as array of digits by given integer number. 
+
+
+using System;
+using System.Numerics;
+
+class NFactorial
+{
+    static BigInteger Factorial(BigInteger number)
+    {
+        BigInteger multiplication = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            multiplication *= i;
+        }
+        return multiplication;
+    }
+
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(Factorial(i));
+        }
+    }
+}
