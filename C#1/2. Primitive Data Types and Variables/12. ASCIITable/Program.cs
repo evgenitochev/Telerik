632116aef89c9ce8ae5334953dc743a5fe5897c6
evgenitochev <evgenitochev@gmail.com>
2013-08-06 18:06:00 +0300

    homework
    homework

---
+++ C#1/2. Primitive Data Types and Variables/12. ASCIITable/Program.cs
@@ -0,0 +1,17 @@
+﻿// Write a program that prints the entire ASCII table of characters on the console.
+
+
+
+using System;
+
+class PrintsASCIITable
+{
+    static void Main()
+    {
+        Console.WriteLine("ASCII");
+        for (byte i = 32; i < 128; i++)
+        {
+            Console.WriteLine((char)i);
+        }
+    }
+}
