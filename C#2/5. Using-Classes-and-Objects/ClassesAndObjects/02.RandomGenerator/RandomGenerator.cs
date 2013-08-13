312714af8513ca2454303929e6154e86556241b7
evgenitochev <evgenitochev@gmail.com>
2013-08-13 15:12:16 +0300

    add USING CLASSES AND OBJECTS
    add USING CLASSES AND OBJECTS

---
+++ C#2/5. Using-Classes-and-Objects/ClassesAndObjects/02.RandomGenerator/RandomGenerator.cs
@@ -0,0 +1,17 @@
+﻿//Write a program that generates and prints to the console 10 random values in the range [100, 200].
+
+
+using System;
+
+class RandomGenerator
+{
+    static void Main()
+    {
+        Random randomNumber = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("{0}", randomNumber.Next(100, 201));
+        }
+    }
+}
---
+++ C#2/5. Using-Classes-and-Objects/ClassesAndObjects/06. SumOfNumString/SumOfNumString.cs
@@ -0,0 +1,23 @@
+﻿//You are given a sequence of positive integer values written into a string, separated by spaces. 
+//Write a function that reads these values from given string and calculates their sum.
+
+using System;
+
+class SumOfNumString
+{
+    static void Main()
+    {
+       
+        //string input = "43 68 9 23 318";
+        string input = Console.ReadLine();
+
+        input = input.Trim();
+        int sum = 0;
+        string[] strNums = input.Split(' ');
+        for (int i = 0; i < strNums.Length; i++)
+        {
+            sum = sum + int.Parse(strNums[i].Trim());
+        }
+        Console.WriteLine(sum);
+    }
+}
