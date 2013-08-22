791337d0599fb8b0080427d64cc0dac52aba99c0
evgenitochev <evgenitochev@gmail.com>
2013-08-22 14:15:02 +0300

    add classes  and objects / add exception handling
    add classes  and objects / add exception handling

---
+++ C#2/6. Exception-Handling/ExceptionHandling/01. SquareRootOfNum/SquareRootOfNum.cs
@@ -0,0 +1,53 @@
+﻿//Write a program that reads an integer number and calculates and prints its square root. 
+//If the number is invalid or negative, print "Invalid number". In all cases finally print "Good bye". 
+//Use try-catch-finally.
+
+using System;
+
+class SquareRootOfNum
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer number: ");
+        try
+        {
+
+            int num = int.Parse(Console.ReadLine());
+            CheckNegNum(num);
+
+            double squareRoot = Math.Sqrt(num);
+            Console.WriteLine("The square root of {0} is {1}.", num, squareRoot);
+
+        }
+        catch (FormatException formatException)
+        {
+
+            throw new FormatException("Invalid number! " + formatException.Message);
+
+        }
+        catch (OverflowException)
+        {
+
+            Console.WriteLine("The input number is too big or too small!");
+
+        }
+
+        finally
+        {
+
+            Console.WriteLine("Goodbye!");
+
+        }
+    }
+
+    static void CheckNegNum(int number)
+    {
+        if (number < 0)
+        {
+
+            throw new ArithmeticException("Invalid number! The square root is defined only for non-negative numbers!");
+
+        }
+
+    }
+}
---
+++ C#2/6. Exception-Handling/ExceptionHandling/04. FileDownload/FileDownload.cs
@@ -0,0 +1,37 @@
+﻿//Write a program that downloads a file from Internet 
+//(e.g. http://www.devbg.org/img/Logo-BASD.jpg) and stores it the current directory.
+//Find in Google how to download files in C#. Be sure to catch all exceptions and to free any used resources in the finally block.
+
+
+using System;
+using System.Linq;
+using System.Net;
+
+class FileDownload
+{
+    static void Main(string[] args)
+    {
+        using (WebClient picture = new WebClient())
+        {
+            try
+            {
+                Console.WriteLine("This program will download the logo of BARS to your desktop.");
+                string path = Environment.ExpandEnvironmentVariables(@"%UserProfile%\Desktop\logo-basd.jpg");
+                picture.DownloadFile("http://www.devbg.org/img/Logo-BASD.jpg", path);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("You entered an empty address.");
+            }
+            catch (WebException we)
+            {
+                Console.WriteLine(we.Message);
+            }
+            catch (NotSupportedException nse)
+            {
+                Console.WriteLine(nse.Message);
+            }
+        }
+
+    }
+}
